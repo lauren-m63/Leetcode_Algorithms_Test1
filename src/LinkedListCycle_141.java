54d8cb37dd93c8831a1a97db27235b4af60ee1b0
lauren-m63 <lauren.m63@yahoo.com>
2026-02-15 13:42:31 -0600

    starting SortColors_75

--- src/LinkedListCycle_141.java
+++ src/LinkedListCycle_141.java
@@ -20,13 +20,61 @@ head is pointing at the first one so maybe make fummy head wiht -1 node again
 
 pos is telling you whcih node the tail is pointing at ohhhhh
 so if pos is null then no cycle okay so confusing for no reason
+
+
+so i just need to go through the list and keep track of all the pointer nodes that have happened can i do a double search or is that too much
+
      */
 
     public static void main(String[] args) {
-
-
-
+       //okay i test in other one its fine
     }// END MAIN
+
+    /*
+    im doing the loopign methohd istead o fmy weird way that does not wrok
+    so i am going to move them through and if they are the same then they will meet0 what if there are duplicates in the list nah they are nodes so its about the pointers not value
+    and it cant skip over bevause even if it misses one time itll just loop until its null or they eventually meet
+    if the cycle exists they will meet even if the while loop has to run a sec longer
+     */
+
+    public static boolean hasCycle(ListNode head){
+
+        if (head == null){
+            return false;
+        }
+
+        ListNode first= head;
+        ListNode second= head;
+
+//        while (first!=null&& first.next!=null){
+//            first =  first.next;
+//            second = second.next.next; // sayign this is null so it cant read but it should be out of while but i guess i decalre inside so let me move
+//
+//            // i put wrong thing in the while loop so now im checking the one its on and the next one to make sure not null then if it finds null it should exit and return false
+//            // i dont need to check first.next.next in while loop bevcuase im skipping overnext.next so i only need the 2 so im checking if next exists and then i am moving 2 again from the one i check okay
+//            if (first==second){
+//                return true;
+//            }
+//        } // end while loop
+
+
+        ListNode linear= head;
+        ListNode twoahead= head;
+
+        while (twoahead!=null && twoahead.next!=null){
+            linear =  linear.next;
+            twoahead = twoahead.next.next;
+
+            if (linear==twoahead){
+                return true;
+            }
+        } // end while loop
+
+
+
+        return false;
+
+    } // end hasCycle method
 
 
 }// LAST BRACKET
---
+++ src/SortColors_75.java
@@ -0,0 +1,16 @@
+public class SortColors_75 {
+
+    public static void main (String[] args) {
+
+    }// END MAIN
+
+
+
+    public void sortColors(int[] nums) {
+
+    }// END SORTCOLORS
+
+
+
+
+} // LAST BRACKET
